b62b7ae9ad2d22583a6d7c59c9a5263743eeff8c
kabak89 <kabak89@gmail.com>
2017-11-13 10:08:35 +0700

    удалил неиспользуемый код

--- app/src/main/java/com/test/kabak/openweather/core/viewModels/CitiesListViewModel.java
+++ app/src/main/java/com/test/kabak/openweather/core/viewModels/CitiesListViewModel.java
@@ -7,7 +7,6 @@ import android.arch.lifecycle.ViewModel;
 
 import com.test.kabak.openweather.core.repositories.CitiesRepository;
 import com.test.kabak.openweather.core.storage.City;
-import com.test.kabak.openweather.core.storage.CurrentWeather;
 
 import java.util.List;
 
@@ -21,42 +20,4 @@ public class CitiesListViewModel extends ViewModel {
             return citiesRepository.getCitiesWeather(input);
         }
     });
-
-
-
-//    LiveData<Resource<List<CurrentWeather>>> listWeather = new MutableLiveData<>();
-//
-//    public CitiesListViewModel() {
-//        citiesRepository.getCities().observe(, new Observer<List<City>>() {
-//            @Override
-//            public void onChanged(@Nullable List<City> cities) {
-//
-//            }
-//        });
-//    }
-//
-//    public LiveData<Resource<List<CurrentWeather>>> getListWeather() {
-//        return listWeather;
-//    }
-
-//    final LiveData<List<City>> cityLiveData = citiesRepository.getCitiesWeather();
-
-//    public final LiveData<CurrentWeather> currentWeatherLiveData =
-//            Transformations.map(cityLiveData, new Function<List<City>, CurrentWeather>() {
-//                @Override
-//                public CurrentWeather apply(List<City> input) {
-//                    CurrentWeather currentWeather = new CurrentWeather();
-//                    return currentWeather;
-//                }
-//            });
-
-    /*public LiveData<Resource<List<CurrentWeather>>> getListWeather() {
-        if(listWeather == null) {
-            listWeather =  citiesRepository.getCitiesWeather();
-        }
-
-        return listWeather;
-    }  */
-
-
 }
