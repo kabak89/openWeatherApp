f7591208c713c4af32e55766519e8ad14b5131d7
Eugeny Kabak <e.kabak@trueengineering.ru>
2022-04-09 11:32:36 +0700

    Перевел gradle скрипты на KTS

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,17 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 buildscript {
-    ext.kotlin_version = '1.6.10'
-
     repositories {
         google()
         jcenter()
-        maven { url 'https://jitpack.io' }
+        maven(url = "https://jitpack.io")
     }
     dependencies {
-        classpath 'com.android.tools.build:gradle:7.1.3'
+        classpath("com.android.tools.build:gradle:${InfrastructureVersions.gradle_plugin}")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${InfrastructureVersions.kotlin}")
     }
 }
 
@@ -24,6 +22,6 @@ allprojects {
     }
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
---
+++ buildSrc/src/main/kotlin/InfrastructureVersions.kt
@@ -0,0 +1,4 @@
+object InfrastructureVersions {
+    const val kotlin = "1.6.10"
+    const val gradle_plugin = "7.1.3"
+}
